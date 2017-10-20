5ec7d83bc3630f4b933cba81e5b6cce4a48a404e
jie cai <jc926@afsconnect1.njit.edu>
2017-10-20 18:06:55 -0400

    change1

--- upload.php
+++ upload.php
@@ -1,46 +1,40 @@
- 
-<html>
-<form method="POST" enctype="multipart/form-data">
-    Select file from your local: <br><br>
-    <input type="file" name="file">
-    <input type="submit" name="upload" value=" Start uploading">
-</form>
-</html>
+ <?php
 
-
-<?php
 
 //turn on debugging messages
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 
+class upload extends page{
+    public function get(){
+     
+      $form = '<form method="POST" enctype="multipart/form-data">';
+      $this->html .= 'Select file from your local: <br><br>';
+      $form .= '<input type="file" name="file">';
+      $form .= '<input type="submit" name="upload" value=" Start uploading">';
+      $form .= '</form>';
+      $this->html .= $form;
+    }
 
-if(isset($_REQUEST["upload"]))
-{
-if (isset($_FILES['file'])) {
+    public function post(){
+        if(isset($_REQUEST["upload"])){
+        if (isset($_FILES['file'])) {
         $file   = $_FILES['file'];
         // print_r($file);  just checking File properties
-
         // File Properties
         $file_name  = $file['name'];
         $file_tmp   = $file['tmp_name'];
         $file_size  = $file['size'];
         $file_error = $file['error'];
-
         // Working With File Extension
         $file_ext   = explode('.', $file_name);
         $file_fname = explode('.', $file_name);
-
         $file_fname = strtolower(current($file_fname));
         $file_ext   = strtolower(end($file_ext));
         $allowed    = array('csv','txt','pdf','doc');
-
-
         if (in_array($file_ext,$allowed)) {
             //print_r($_FILES);
-
-
             if ($file_error === 0) {
                 if ($file_size <= 5000000) {
                         $file_name_new     =  $file_fname . '.' . $file_ext;
@@ -48,27 +42,20 @@ if (isset($_FILES['file'])) {
                         $file_destination =  'uploadfolder/' . $file_name_new;
                         // echo $file_destination;
                         if (move_uploaded_file($file_tmp, $file_destination)) {
-                                echo "Cv uploaded";
+                                echo "File uploaded";
                         }
-                        else
-                        {
-                            echo "some error in uploading file".mysql_error();
-                        }
-//                        
-                }
-                else
-                {
-                    echo "size must bne less then 5MB";
+                        
                 }
             }
+        }
+        }
+        }
+    }  
 
-        }
-        else
-        {
-            echo "invalid file";
-        }
+
 }
-}
+
 
 ?>
+    
 
